e8cbb1d354e028813caf650f2022797261cd83a1
Wiciy <witoldrerych@o2.pl>
2024-02-22 15:01:40 +0100

    Initial commit

---
+++ Biblioteka/Book.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    class Book
+    {
+        //jezeli chcemy kategorie, to nie tworzymy kurwa nastepnej funkcji, tylko mozemy tutaj dodac atrybut
+        public string Tytul;
+        public string Autor;
+        public int RokWydania;
+        public int LiczbaStron;
+        public bool Dostepna;
+
+        public Book()
+        {
+            Tytul = "Brak";
+            Autor = "Brak";
+            RokWydania = 0;
+            LiczbaStron = 0;
+            Dostepna = true;
+        }
+
+        public Book(string tytul, string autor, int rok, int liczba, bool dostepna)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            RokWydania = rok;
+            LiczbaStron = liczba;
+            Dostepna = dostepna;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Tytul + ", " + Autor + ", " + RokWydania + ", " + LiczbaStron);
+        }
+
+        public void ModifyInfo()
+        {
+            Console.WriteLine("Ile zmiennych będziesz modyfikował? (max 4)");
+            int x = int.Parse(Console.ReadLine());
+            if (x>4)
+            {
+                Console.WriteLine("Podales za duza liczbe.");
+            }
+            else
+            {
+                for(int i=1; i==x; i++)
+                {
+                    Console.WriteLine("Dane ktorej zmiennej bedziesz modyfikowal? (tytul, autor, rok wydania, liczba stron)");
+                    string zmiennam = Console.ReadLine();
+                    if (zmiennam.ToLower() == "tytul")
+                    {
+                        Console.WriteLine("Podaj tytul: ");
+                        Tytul = Console.ReadLine();
+                    }
+                    else if (zmiennam.ToLower() == "autor")
+                    {
+                        Console.WriteLine("Podaj autora: ");
+                        Autor = Console.ReadLine();
+                    }
+                    else if (zmiennam.ToLower() == "rok wydania")
+                    {
+                        Console.WriteLine("Podaj rok wydania: ");
+                        RokWydania = int.Parse(Console.ReadLine());
+                    }
+                    else if (zmiennam.ToLower() == "liczba stron")
+                    {
+                        Console.WriteLine("Podaj liczbe stron: ");
+                        LiczbaStron = int.Parse(Console.ReadLine());
+                    }
+                    else
+                        Console.WriteLine("Wprowadzona zla nazwe zmiennej");
+                }
+            }
+        }
+    }
+    class PaperBook : Book
+    {
+
+    }
+    class EBook : Book
+    {
+
+    }
+    class Library : Book
+    {
+        public List<Book> books;
+        public Library()
+        {
+            books = new List<Book>();
+        }
+        public void AddBook(Book obj)
+        {
+            books.Add(obj);
+        }
+        public void RemoveBook(Book obj)
+        {
+            books.Remove(obj);
+        }
+        public void SearchBook(string wyszukiwane) //Try catch, bo w pierwszej wersji metody, string z inputa był konwertowany na int i to wywalało exception
+        {
+            for (int i = 0; i < books.Count(); i++)
+            {
+                try
+                {
+                    if (books[i].RokWydania == Convert.ToInt32(wyszukiwane))
+                    {
+                        books[i].ShowInfo();
+                    }
+                    else if (books[i].LiczbaStron == Convert.ToInt32(wyszukiwane))
+                    {
+                        books[i].ShowInfo();
+                    }
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+                    if (books[i].Tytul == wyszukiwane)
+                    {
+                        books[i].ShowInfo();
+                    }
+                    else if (books[i].Autor == wyszukiwane)
+                    {
+                        books[i].ShowInfo();
+                    }
+                }
+            }
+        }
+    }
+    class Reader : Library
+    {
+        public string Imie;
+        public string Nazwisko;
+        public string NumerKarty;
+        public string BDate;
+        public string RDate;
+        public DateTime WhenTR; //When to return
+        public List<Book> wyporzyczenia;
+
+        public Reader()
+        {
+            wyporzyczenia = new List<Book>();
+        }
+
+        public DateTime GetCurrentDate()
+        {
+            return DateTime.Now;
+        }
+        public void BorrowBook(Book obj) //Metoda przypisuje date wyporzyczenia, zmienia dostepnosc i dodaje ksiazke do listy wyporzyczonych
+        {
+            if (obj.Dostepna == true && wyporzyczenia.Count < 10)
+            {
+                DateTime date = GetCurrentDate();
+                BDate = date.ToString("dd - MM - yyyy");
+                WhenTR = date.AddDays(10);  //W tej bibliotece masz 10 dni na zwrot ksiazki
+
+                obj.Dostepna = false;
+                wyporzyczenia.Add(obj);
+                Console.WriteLine("Ksiazka zostala wyporzyczona.");
+            }
+            //ksiazka musi byc dostepna, a limit wyporzyczen wynosi 10 ksiazek
+            else if (Dostepna == false || wyporzyczenia.Count == 10)
+            {
+                Console.WriteLine("Ksiazka niedsotepna do wyporzyczenia.");
+            }
+        }
+        public void ReturnBook(Book obj)
+        {
+            if (wyporzyczenia.Contains(obj))
+            {
+                DateTime date = GetCurrentDate();
+                RDate = date.ToString("dd - MM - yyyy");
+
+                obj.Dostepna = true;
+                wyporzyczenia.Remove(obj);
+                Console.WriteLine("Oddales ksiazke.");
+            }
+            else
+            {
+                Console.WriteLine("Nie udalo Ci sie oddac ksiazki.");
+            }
+        }
+    }
+    class Borrow : Reader
+    {
+        //!stringi nie przechowują dat!!!!!!!!!!!
+        public string BorrowDate;
+        public string ReturnDate;
+        public void LongerBorrow(double days)
+        {
+            WhenTR.AddDays(days);
+        }
+    }
+}
---
+++ Biblioteka/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library1 = new Library();
+            Borrow borrow1 = new Borrow();
+            Reader reader1 = new Reader();
+
+            Book book1 = new Book("Odwet", "Moe Gerni", 1678, 670, true);
+            Book book2 = new Book("Dzieje", "Pablo Pablo", 1669, 51, true);
+            Book book3 = new Book("Pokonaj mnie!", "Ejczu Ebezoga", 2015, 320, true);
+            Book book4 = new Book();
+            Book book5 = new Book();
+            Book book6 = new Book();
+            Book book7 = new Book();
+            Book book8 = new Book();
+            Book book9 = new Book();
+            Book book10 = new Book();
+            Book book11 = new Book();
+            //book1.ShowInfo();
+            library1.AddBook(book1);
+            library1.AddBook(book2);
+            library1.AddBook(book3);
+            library1.SearchBook("1669");
+
+            /*foreach (Book book in library1.books)
+            {
+                Console.WriteLine(book.Tytul + ", " + book.Autor + ", " + book.RokWydania + ", " + book.LiczbaStron + ", " + book.Dostepna);
+            }*/
+
+            /*reader1.BorrowBook(book1);
+            Console.WriteLine(book1.Dostepna);
+
+            reader1.ReturnBook(book1);
+            reader1.ReturnBook(book2);
+
+            Console.WriteLine(reader1.BDate);
+            Console.WriteLine(reader1.WhenTR);
+            Console.WriteLine(reader1.RDate);
+            Console.WriteLine(library1.books.Count);*/
+
+            /*reader1.BorrowBook(book1);
+            reader1.BorrowBook(book2);
+            reader1.BorrowBook(book3);
+            reader1.BorrowBook(book4);
+            reader1.BorrowBook(book5);
+            reader1.BorrowBook(book6);
+            reader1.BorrowBook(book7);
+            reader1.BorrowBook(book8);
+            reader1.BorrowBook(book9);
+            reader1.BorrowBook(book10);
+            reader1.BorrowBook(book11);*/
+
+            Console.ReadKey();
+        }
+    }
+}
